e562621a95df160729af9c2a5353ee3a483adcd7
JakubBilski1 <jakub.bilski@spoko.pl>
2023-09-30 21:17:34 +0200

    a little cleanup

---
+++ zadania z lekcji 29.09.2023/zadanie1/index.js
@@ -0,0 +1,11 @@
+const http = require('http')
+
+const server = http.createServer((req, res)=>{
+    res.end("Hello World!")
+})
+
+const PORT = 3000
+
+server.listen(PORT, ()=>{
+    console.log(`Server listening on port: ${PORT}`)
+})
