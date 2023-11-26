0cddbec7527d619e4248bc0968e5776afb355659
JakubBilski1 <jakub.bilski@spoko.pl>
2023-11-26 19:32:55 +0100

    updated index

--- zadania-z-lekcji-17-11/CRUDApp/server/index.ts
+++ zadania-z-lekcji-17-11/CRUDApp/server/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
-import homeRoute from './Routes/homeRoute';
+import userRoutes from './Routes/userRoutes';
 
 const app = express();
 const port = 5000;
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', homeRoute);
+app.use('/u', userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
