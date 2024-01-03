1485f1b6dc6f4aef884c9a3a9237cfbe2f7fb163
JakubBilski1 <jakub.bilski@spoko.pl>
2024-01-03 15:43:28 +0100

    new pages

---
+++ zadanie-z-lekcji-03-01/zadanie/src/pages/Blog/page.tsx
@@ -0,0 +1,9 @@
+function Blog(): JSX.Element {
+  return (
+    <>
+      <h1>Blog</h1>
+    </>
+  )
+}
+
+export default Blog
---
+++ zadanie-z-lekcji-03-01/zadanie/src/pages/Home/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from "../../Components/NavBar/navbar"
+
+function Home(): JSX.Element {
+  return (
+    <>
+        <NavBar />
+    </>
+  )
+}
+
+export default Home
