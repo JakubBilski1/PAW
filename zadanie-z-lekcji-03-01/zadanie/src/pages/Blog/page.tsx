97dd73b697f86e6cbed43a249bb363696d42f2c0
JakubBilski1 <jakub.bilski@spoko.pl>
2024-01-10 15:34:38 +0100

    blog component updated

--- zadanie-z-lekcji-03-01/zadanie/src/pages/Blog/page.tsx
+++ zadanie-z-lekcji-03-01/zadanie/src/pages/Blog/page.tsx
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react'
+import { Post } from '../../types'
+import { Link } from 'react-router-dom'
+
 function Blog(): JSX.Element {
+  const [post, setPost] = useState<Post[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+  useEffect(() => {
+    setLoading(true)
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json() as Promise<Post[]>)
+      .then((data) => {
+        setPost(data)
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+      .finally(()=>{
+        setLoading(false)
+      })
+  }, [])
   return (
     <>
-      <h1>Blog</h1>
+      {loading && <p>Loading...</p>}
+      {error ? <p>{error}</p> :
+      <ul>
+        {post.map((post) => {
+          return (
+            <li key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+              <Link to={`/blog/${post.id}`}>Read more</Link>
+            </li>
+          )
+        })}
+      </ul>}
     </>
   )
 }
