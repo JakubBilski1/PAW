5af368bd993fcf31002a53092317903aab532e18
JakubBilski1 <jakub.bilski@spoko.pl>
2024-01-25 20:32:45 +0100

    layout and router finished

---
+++ zadania-z-lekcji-17-01/zadanie/src/App.tsx
@@ -0,0 +1,25 @@
+import "../src/App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { routes } from "./helpers/routes"
+import NavBar from "./components/NavBar/NavBar"
+import Header from "./components/Header/Header"
+
+function App(): JSX.Element {
+  return (
+    <>
+      <Header />
+      <main>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            {routes.map((route) =>(
+              <Route key={route.path} path={route.path} element={route.element} />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </>
+  )
+}
+
+export default App
