ea4120c6f69d5aa0c8fd61954f25231b5f35e9d7
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-28 15:20:13 +0100

    BlogPost update

--- zadanie-z-lekcji-03-01/zadanie/src/Components/BlogPost.tsx
+++ zadanie-z-lekcji-03-01/zadanie/src/Components/BlogPost.tsx
@@ -1,31 +1,14 @@
-import { Post } from "../types";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CommentList from "./CommentList";
+import { usePost } from "../services/usePost";
 
 export default function BlogPost(){
     const { id } = useParams<{id: string}>();
-
-    const [post, setPost] = useState<Post | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string>("");
-
-    useEffect(() => {
-        setLoading(true);
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-            .then((response) => response.json() as Promise<Post>)
-            .then((data) => setPost(data))
-            .catch((error) => {
-                setError(error.message);
-            })
-            .finally(() => {
-                setLoading(false);
-            });
-    }, [id]);
+    const { data: post, isLoading: loading, error } = usePost(id || "");
     return (
         <>
             {loading && <p>Loading...</p>}
-            {error ? <p>{error}</p> :
+            {error ? <p>error occured</p> :
             <ul>
                 {post && (
                     <li key={post.id}>
