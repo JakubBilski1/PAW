4859e0bb139fe1d5c04a6e46bc891984a64f98b8
JakubBilski1 <jakub.bilski@spoko.pl>
2024-02-28 15:19:36 +0100

    query client + devtools

--- zadanie-z-lekcji-03-01/zadanie/src/App.tsx
+++ zadanie-z-lekcji-03-01/zadanie/src/App.tsx
@@ -1,19 +1,25 @@
 import "../src/App.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { routes } from "./Helpers/routes"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 
 function App(): JSX.Element {
+  const queryClient = new QueryClient()
   return (
     <>
-      <BrowserRouter>
-       <Routes>
-        {routes.map((route) => {
-          return(
-            <Route key={route.path} path={route.path} element={route.element} />
-          )
-        })}
-       </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+        <Routes>
+          {routes.map((route) => {
+            return(
+              <Route key={route.path} path={route.path} element={route.element} />
+            )
+          })}
+        </Routes>
+        </BrowserRouter>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </>
   )
 }
